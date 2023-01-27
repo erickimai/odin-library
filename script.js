a25e3571981e8e1b2c1ea5993dc8032cb8131fee
Erick <erick.imai.rodrigues@gmail.com>
2023-01-26 23:54:39 -0300

    Add css

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-const a = 0;
+const myLibrary = [];
+const form = document.querySelector("form");
+const bname = document.getElementById("bname");
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
+  };
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+function displayBooks() {}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  addBookToLibrary(bname.value);
+  bname.value = "";
+});
