354e4ef8007a203714c62c3baad06a35b0656bca
Erick <erick.imai.rodrigues@gmail.com>
2023-01-29 14:59:24 -0300

    Finish general layout

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const myLibrary = [];
 const form = document.querySelector("form");
-const bname = document.getElementById("bname");
+const tbody = document.querySelector("tbody");
 
-function Book(title, author, pages, read) {
+function Book(title, author, pages, year) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.read = read;
+  this.year = year;
   this.info = function () {
     return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`;
   };
@@ -16,10 +16,56 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
-function displayBooks() {}
+function displayBook(book) {
+  const row = document.createElement("tr");
+  const name = document.createElement("td");
+  name.innerText = `${book.title}`;
+  const author = document.createElement("td");
+  author.innerText = `${book.author}`;
+  const pages = document.createElement("td");
+  pages.innerText = `${book.pages}`;
+  const year = document.createElement("td");
+  year.innerText = `${book.year}`;
+  const read = document.createElement("td");
+  const select = document.createElement("select");
+  read.append(select);
+  const yes = document.createElement("option");
+  yes.innerText = "Yes";
+  select.append(yes);
+  const notYet = document.createElement("option");
+  notYet.innerText = "Not Yet";
+  select.append(notYet);
+  const remove = document.createElement("td");
+  const removeButton = document.createElement("button");
+  removeButton.innerText = "Delete";
+  removeButton.addEventListener("click", (e) => {
+    e.currentTarget.parentNode.parentNode.remove();
+  });
+  row.append(name);
+  row.append(author);
+  row.append(pages);
+  row.append(year);
+  row.append(read);
+  row.append(remove);
+  remove.append(removeButton);
+  tbody.append(row);
+}
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  addBookToLibrary(bname.value);
-  bname.value = "";
+  const book = new Book(
+    form.elements.btitle.value,
+    form.elements.bauthor.value,
+    form.elements.bpages.value,
+    form.elements.byear.value
+  );
+  displayBook(book);
 });
+
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
